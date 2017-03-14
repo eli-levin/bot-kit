650e8abe75bc195b98a314526a3f630be4f084e5
Eli Levin <eli.j.wasserman@gmail.com>
2017-03-14 13:49:28 -0500

    exporting bot

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-exports.poop = () => {
-    console.log('hey');
+/////////////////////////
+// index.js
+// 2017 (c) Eli Levin
+// bot-kit
+/////////////////////////
+
+'use strict'
+
+module.exports = {
+    'Bot' : Bot,
+    'Intent': Intent
 };
