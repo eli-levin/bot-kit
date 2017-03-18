9ae0350cd7ef18979a92141f492155ea548b8e0a
Eli Levin <eli.j.wasserman@gmail.com>
2017-03-17 23:39:58 -0500

    tests trainData so far...

--- test/BotTest.js
+++ test/BotTest.js
@@ -6,6 +6,10 @@
 
 'use strict'
 
+const Bot    = require('../lib/Bot.js'),
+      Cdata  = require('./data/ClassificationData.js'),
+      assert = require('assert');
+
 //
 // Test the bot
 //
@@ -13,6 +17,15 @@ describe('Bot', () => {
     describe('#trainBot(trainingData, testData)', () => {
         it('Should return a successful training response.', () => {
             //-- Begin test call
+            let weatherBot = new Bot([/*todo: fill this for next method test*/]);
+            // todo: maybe trainBot should return some sort of success/failure metric instead of void
+            try{
+                weatherBot.trainBot(Cdata.trainingData, Cdata.testData);
+                assert(true, "");
+            }catch(e){
+                console.error(e);
+                assert(false, "Bot failed training.");
+            }
             //-- End test call
         });
     });
